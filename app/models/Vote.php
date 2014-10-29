af9861f6754ed1f2490b272b9d39e8e06fef277f
Arktri <cjpaterson.3@gmail.com>
2014-10-29 15:55:06 +0000

    Stopped tracking vendor and module_materials files.  Added migration to add created_at and updated_at to votes table.

---
+++ app/models/Vote.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Collection;
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class User extends Eloquent implements UserInterface, RemindableInterface {
+
+    use UserTrait, RemindableTrait;
+
+    protected $table = 'users';
+    public $timestamps = false;
+    protected $guarded = ['id'];
+
+
+}
