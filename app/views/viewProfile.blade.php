4a823be7444d7336983390acec42ef68c69b4487
Ryan Gibbs <ryangibbs11@msn.com>
2014-10-31 14:44:14 +0000

    added module name to profile public notes.

--- app/views/viewProfile.blade.php
+++ app/views/viewProfile.blade.php
@@ -43,7 +43,7 @@
             @foreach ($publicNotes as $publicNote)
                 <div class="panel panel-default individual-public-note-container" data-id="{{$publicNote->id}}">
                     <div class="panel-heading public-note-head">
-                        <span class="public-note-title">{{{$publicNote->note_title}}}</span>
+                        <span class="public-note-title"><strong>[{{{ Module::find($publicNote->module_id)->module_name }}}]</strong> {{{ $publicNote->note_title}}}</span>
                         <span class="pull-right ">
                             <span class="public-notes-created-time">last changed: {{$publicNote->updated_at->diffForHumans()}}</span>
                             <i class="glyphicon glyphicon-chevron-down public-note-icon"></i>
