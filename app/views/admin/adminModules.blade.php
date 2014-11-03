07bbf0786cb1fcbb09a2dec4489c50d2ae503774
Ryan Gibbs <ryangibbs11@msn.com>
2014-11-03 14:38:22 +0000

    Adding new module kind of works, still need to fix some stuff.

--- app/views/admin/adminModules.blade.php
+++ app/views/admin/adminModules.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-sm-12">
         <h2>Add Module</h2>
-        {{ Form::open(array('method' => 'POST', 'action' => 'CourseController@store', 'id' => 'admin-module-form')) }}
+        {{ Form::open(array('method' => 'POST', 'action' => 'ModuleController@store', 'id' => 'admin-module-form')) }}
             {{ Form::input('text', 'module_name', '', ['class'=>'form-control', 'placeholder' => 'Name', 'required']) }} <br>
             {{ Form::textarea('module_description', '', ['class'=>'form-control', 'rows' => '8', 'placeholder' => 'Description', 'required']) }} <br>
             {{ Form::select('module_course', ($courses), '', ['class' => 'form-control', 'required']) }} <br>
