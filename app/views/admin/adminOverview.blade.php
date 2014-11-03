10d934b0228cf83cb39d5044dafebfd40969fcfb
Ryan Gibbs <ryangibbs11@msn.com>
2014-11-03 15:07:30 +0000

    lined up text.

--- app/views/admin/adminOverview.blade.php
+++ app/views/admin/adminOverview.blade.php
@@ -1,5 +1,6 @@
 <div class="row">
-	<p>Welcome to the GRID <strong>{{ Auth::user()->name }}</strong>,</p>
-
-	<p>Use the tabs above to add users, modules or courses.</p>
+	<div class="col-sm-12">
+		<p>Welcome to the GRID <strong>{{ Auth::user()->name }}</strong>,</p>
+		<p>Use the tabs above to add users, modules or courses.</p>
+	</div>
 </div>
