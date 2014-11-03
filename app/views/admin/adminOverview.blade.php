639e6dd93af84a472b7169aa342ccf7b7fae6886
Ryan Gibbs <ryangibbs11@msn.com>
2014-11-03 15:03:43 +0000

    changed text

--- app/views/admin/adminOverview.blade.php
+++ app/views/admin/adminOverview.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-	<p>Welcome <strong>{{ Auth::user()->name }}</strong>,</p>
+	<p>Welcome to the GRID <strong>{{ Auth::user()->name }}</strong>,</p>
 
 	<p>Use the tabs above to add users, modules or courses.</p>
 </div>
