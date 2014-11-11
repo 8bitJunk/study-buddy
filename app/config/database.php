8a95aefa694c699d19ee372f3c4b9fdbd3d570d5
Christopher Paterson <cjpaterson.3@gmail.com>
2014-11-11 13:51:47 +0000

    updated production server

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'thesite',
+			'username'  => 'root',
+			'password'  => 'droogdroog',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
